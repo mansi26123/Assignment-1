752cc89c3f20c5f339fd20ebd19e4f319314c94d
mansi26123 <98789843+mansi26123@users.noreply.github.com>
2022-02-01 15:46:16 -0800

    Assignment 1 commit

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Assignment-1
- CS648 Assignment 1
+
+This is the first assignment of the CS 648.02 which deals with the MERN stack development.
---
+++ public/App.js
@@ -0,0 +1,29 @@
+"use strict";
+
+var nameComponent = /*#__PURE__*/React.createElement("div", {
+  id: "out"
+}, /*#__PURE__*/React.createElement("h2", {
+  id: "name_comp"
+}, "Mansi Vyas"));
+var pictureComponent = /*#__PURE__*/React.createElement("div", {
+  id: "picture_out"
+}, /*#__PURE__*/React.createElement("img", {
+  id: "picture_comp",
+  src: "./assignment_1.png"
+}));
+var introComponent = /*#__PURE__*/React.createElement("div", {
+  id: "intro_out"
+}, /*#__PURE__*/React.createElement("p", {
+  id: "intro_comp"
+}, "Lorem ipsum dolor sit amet,**** consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."));
+var buttonComponent = /*#__PURE__*/React.createElement("div", {
+  id: "button_out"
+}, /*#__PURE__*/React.createElement("a", {
+  href: "https://github.com/mansi26123"
+}, /*#__PURE__*/React.createElement("button", {
+  id: "button_comp"
+}, "VIEW MY GITHUB REPO")));
+ReactDOM.render(nameComponent, document.getElementById('name'));
+ReactDOM.render(pictureComponent, document.getElementById('picture'));
+ReactDOM.render(introComponent, document.getElementById('introduction'));
+ReactDOM.render(buttonComponent, document.getElementById('button'));
